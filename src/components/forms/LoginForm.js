ee3083c9c72ea1624dce097ac68ae41d1b4707f3
kingHyen <modan8199@gmail.com>
2024-09-20 15:32:30 +0900

    - hoxfix - email phone birthday 입력오류 해결

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+import '../../css/LoginForm.css'; 
 import { callLoginAPI } from "../../apis/UserAPICalls";
 import { resetLoginUser } from "../../modules/UserModule";
 
 function LoginForm() {
-
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { user, error } = useSelector(state => state.userReducer);
@@ -16,7 +15,6 @@ function LoginForm() {
         password: ''
     });
 
-    // 입력창 변화 감지하여 값 담기
     const onChangeHandler = e => {
         setLoginInfo({
             ...loginInfo,
@@ -31,30 +29,37 @@ function LoginForm() {
     useEffect(() => {
         if (error) {
             alert(error);
-
-            setLoginInfo({
-                email: '',
-                password: ''
-            });
-
+            setLoginInfo({ email: '', password: '' });
             dispatch(resetLoginUser());
         } else if (user) {
             localStorage.setItem("isLogin", true);
             localStorage.setItem("user", JSON.stringify(user));
-            navigate("/");
+            navigate("/"); // 로그인 성공 시 메인 페이지로 이동
         }
     }, [user, error, navigate, dispatch]);
 
     return (
-        <>
-            <label>ID : </label>
-            <input type="text" name="email" value={loginInfo.email} onChange={onChangeHandler} />
-            &nbsp; &nbsp; &nbsp;
-            <label>PASSWORD : </label>
-            <input type="text" name="password" value={loginInfo.password} onChange={onChangeHandler} />
-            &nbsp; &nbsp; &nbsp;
-            <button onClick={onClickHandler}>로그인</button>
-        </>
+        <div className="login-form">
+            <div className="input-group">
+                <label>ID:</label>
+                <input type="text" name="email" value={loginInfo.email} onChange={onChangeHandler} />
+            </div>
+            <div className="input-group">
+                <label>PASSWORD:</label>
+                <input type="password" name="password" value={loginInfo.password} onChange={onChangeHandler} />
+            </div>
+            <button className="button" onClick={onClickHandler}>로그인</button>
+            
+            <div className="links">
+                <div className="signup-link">
+                    <a href="/holdup/signup">회원가입</a>
+                </div>
+                <div className="find-links">
+                    <a href="/find-id">아이디 찾기</a>
+                    <a href="/find-password">비밀번호 찾기</a>
+                </div>
+            </div>
+        </div>
     );
 }
 
