c87dbf0c2c7c808d4363894bc59ee7971d6475e0
kingHyen <modan8199@gmail.com>
2024-09-23 11:02:18 +0900

    - 회원가입 form 을 수정   - 이메일 도메인을 선택하여 입력   - 이용약관 팝업생성   - css 수정 ( button , input )   - TermsPopup.js , css ( 이용약관 ) page

--- src/components/forms/AddressPopup.js
+++ src/components/forms/AddressPopup.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import '../../css/AddressPopup.css'; 
+import { useEffect } from 'react';
 
 const AddressPopup = ({ onAddressSelect }) => {
     useEffect(() => {
@@ -8,22 +7,20 @@ const AddressPopup = ({ onAddressSelect }) => {
             script.src = '//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
             script.onload = () => {
                 window.daum.postcode.load(() => {
-                    document.getElementById('search-address-button').onclick = () => {
-                        new window.daum.Postcode({
-                            oncomplete: function(data) {
-                                const roadAddr = data.roadAddress;
-                                const jibunAddr = data.jibunAddress;
-                                const zipCode = data.zonecode;
+                    new window.daum.Postcode({
+                        oncomplete: function(data) {
+                            const roadAddr = data.roadAddress;
+                            const jibunAddr = data.jibunAddress;
+                            const zipCode = data.zonecode;
 
-                                onAddressSelect({
-                                    roadFullAddr: roadAddr,
-                                    jibunAddr: jibunAddr,
-                                    zipNo: zipCode,
-                                    addressDetail: '' // 여기에 상세주소를 추가할 수 있습니다.
-                                });
-                            }
-                        }).open();
-                    };
+                            onAddressSelect({
+                                roadFullAddr: roadAddr,
+                                jibunAddr: jibunAddr,
+                                zipNo: zipCode,
+                                addressDetail: ''
+                            });
+                        }
+                    }).open();
                 });
             };
             document.body.appendChild(script);
@@ -32,17 +29,7 @@ const AddressPopup = ({ onAddressSelect }) => {
         loadDaumPostcodeScript();
     }, [onAddressSelect]);
 
-    return (
-        <div className="address-search-container">
-            <h2>주소 검색</h2>
-            <input type="text" id="sample4_postcode" placeholder="우편번호" readOnly />
-            <input type="button" id="search-address-button" value="우편번호 찾기" /><br />
-            <input type="text" id="sample4_roadAddress" placeholder="도로명주소" readOnly />
-            <input type="text" id="sample4_jibunAddress" placeholder="지번주소" readOnly />
-            <input type="text" id="sample4_detailAddress" placeholder="상세주소" />
-            <input type="text" id="sample4_extraAddress" placeholder="참고항목" />
-        </div>
-    );
+    return null; // UI를 렌더링하지 않음
 };
 
 export default AddressPopup;
