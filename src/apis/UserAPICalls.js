9e4ec4147ddb692ab3e2fb2c1df770ad4e8add5d
wdh970616 <wdh970616@naver.com>
2024-09-27 12:47:23 +0900

    로그인 상태에 따라 헤더 동적 변경

--- src/apis/UserAPICalls.js
+++ src/apis/UserAPICalls.js
@@ -8,7 +8,7 @@ export function callLoginAPI(loginInfo, navigate) {
             const response = await request("POST", "/login", loginInfo);
 
             console.log("Response 응답로그 확인", response);
-            
+
             // 서버에서 로그인 성공 시 JWT 토큰 반환
             if (response.token) {
                 // 필요한 사용자 정보만 추출
@@ -23,10 +23,8 @@ export function callLoginAPI(loginInfo, navigate) {
                 sessionStorage.setItem("token", response.token); // JWT 토큰 저장
                 sessionStorage.setItem("isLogin", true); // 로그인 상태 저장
                 dispatch(loginSuccess(userData)); // Redux 상태 업데이트
-                
+
                 navigate("/"); // 로그인 성공 후 메인 페이지로 이동
-               
-                return true; // 로그인 성공 시 true 반환
             } else {
                 dispatch(loginFail("아이디와 비밀번호를 확인해주세요."));
             }
@@ -34,5 +32,5 @@ export function callLoginAPI(loginInfo, navigate) {
         } catch (error) {
             dispatch(loginFail(error.message || '로그인 실패'));
         }
-    }
+    };
 }
--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -1,12 +1,19 @@
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux"; // useSelector 추가
+import { resetLoginUser } from "../../modules/UserModule"; // resetLoginUser 액션 임포트
 import style from "../../css/Header.module.css";
 
 function Header() {
 
-    const logout = () => {
-        dispatch(resetLoginUser()); // Redux 상태 초기화
-        sessionStorage.removeItem("isLogin"); // 세션 스토리지 업데이트
-        sessionStorage.removeItem("user"); // 세션 스토리지 업데이트
+    const dispatch = useDispatch(); // dispatch 훅 사용
+    const isLogin = useSelector(state => state.userReducer.isLogin); // Redux에서 로그인 상태 가져오기
+
+    // 로그아웃 처리
+    const logoutHandler = () => {
+        sessionStorage.removeItem("isLogin");
+        sessionStorage.removeItem("user");
+        dispatch(resetLoginUser()); // 상태 초기화
+        window.location.reload(); // 페이지 새로 고침
     };
 
     return (
@@ -24,24 +31,24 @@ function Header() {
             </div>
 
             <div className={style.memberSection}>
-                {sessionStorage.getItem("isLogin") === "true" ? (
+                {isLogin ? ( // Redux에서 가져온 isLogin 상태 사용
                     <>
-                        <NavLink className={style.memberLink} to="/mypage">마이페이지</NavLink>
+                        <NavLink className={style.memberLink} to="/holdup/mypage">마이페이지</NavLink>
                         <span>|</span>
-                        <NavLink className={style.memberLink} to="/chat">내 채팅</NavLink>
+                        <NavLink className={style.memberLink} to="/holdup/chat">내 채팅</NavLink>
                         <span>|</span>
-                        <span className={style.logout} onClick={logout}>로그아웃</span>
+                        <span onClick={logoutHandler} className={style.logout}>로그아웃</span>
                     </>
                 ) : (
                     <>
-                        <NavLink className={style.memberLink} to="holdup/login">로그인</NavLink>
+                        <NavLink className={style.memberLink} to="/holdup/login">로그인</NavLink>
                         <span>|</span>
-                        <NavLink className={style.memberLink} to="holdup/signup">회원가입</NavLink>
+                        <NavLink className={style.memberLink} to="/holdup/signup">회원가입</NavLink>
                     </>
                 )}
             </div>
         </header>
-    );
+    )
 }
 
 export default Header;
