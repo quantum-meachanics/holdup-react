324e57886ca68b232935f07328204807b6ad8957
wdh970616 <wdh970616@naver.com>
2024-10-02 10:17:40 +0900

    이미지 조회까지 완료

--- src/components/forms/SpaceDetailForm.js
+++ src/components/forms/SpaceDetailForm.js
@@ -1,26 +1,25 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { callSpaceDetailAPI } from "../../apis/SpaceAPICalls";
 
 function SpaceDetailForm() {
 
     const { id } = useParams();
-    const navigate = useNavigate();
     const dispatch = useDispatch();
     const { spaceDetail, error } = useSelector(state => state.spaceDetailReducer);
-    const [loadedImages, setLoadedImages] = useState(0);
 
     useEffect(() => {
         dispatch(callSpaceDetailAPI(id));
-    }, [dispatch, id]);
+        console.log("불러온 공간글 객체", {spaceDetail});
+    }, [id, dispatch]);
 
     if (error) return (<div>에러 발생 : {error}</div>);
 
     return (
         <>
             {spaceDetail ? (
-                <div style={{display: "flex", flexDirection: "column"}}>
+                <div style={{ display: "flex", flexDirection: "column" }}>
                     <span>공간명 : {spaceDetail.name}</span>
                     <span>등록자 : {spaceDetail.ownerNickname}</span>
                     <span>등록일시 : {spaceDetail.createDate}</span>
@@ -34,11 +33,15 @@ function SpaceDetailForm() {
                     <span>재고 : {spaceDetail.count}개</span>
                     <span>가격 : {spaceDetail.price} 크레딧</span>
                     <div>
-                    {spaceDetail.imageUrl && spaceDetail.imageUrl.length > 0 ? (
-                                    <img src={spaceDetail.imageUrl} />
-                                ) : (
-                                    <p>이미지가 없습니다.</p>
-                                )}
+                        {spaceDetail.imageUrls && spaceDetail.imageUrls.length > 0 ? (
+                            spaceDetail.imageUrls.map((url, index) => (
+                                <div key={index}>
+                                    <img src={url} alt={`${index}번 이미지`} />
+                                </div>
+                            ))
+                        ) : (
+                            <p>이미지가 없습니다.</p>
+                        )}
                     </div>
                 </div>
             ) : (
